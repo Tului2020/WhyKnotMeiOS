810e040d2d554137361e18d5e51df1f60f35f480
Tului Gantulga <tgantulga@ucdavis.edu>
2021-05-27 15:30:10 -0700

    Initial Commit

---
+++ WhyKnotMe_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhyKnotMe_iOS
+//
+//  Created by Tului Gantulga on 5/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhyKnotMe_iOS/WhyKnotMe_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyKnotMe_iOSApp.swift
+//  WhyKnotMe_iOS
+//
+//  Created by Tului Gantulga on 5/27/21.
+//
+
+import SwiftUI
+
+@main
+struct WhyKnotMe_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
