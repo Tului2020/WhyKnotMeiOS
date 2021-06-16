75cd4a63e3e448588fe4ea5eab8d47edaaba6d94
Tului Gantulga <tgantulga@ucdavis.edu>
2021-06-16 11:04:30 -0700

    working starting over with radio buttons

--- WhyKnotMe_iOS/Utils/RadioButton/RadioButtonField.swift
+++ WhyKnotMe_iOS/Utils/RadioButton/RadioButtonField.swift
@@ -8,68 +8,74 @@
 import SwiftUI
 
 //MARK:- Single Radio Button Field
-//struct RadioButtonField: View {
-//    let id: String
-//    let label: String
-//    let size: CGFloat
-//    let color: Color
-//    let textSize: CGFloat
-//    let isMarked:Bool
+struct RadioButtonField: View {
+    var id: String;
+    var label: String;
+    var size: CGFloat;
+    var color: Color;
+    var textSize: CGFloat;
+    var isMarked: Binding<Bool>;
+    
+    init(<#parameters#>) {
+        <#statements#>
+    }
+    
+    
+    
+    
+}
+
+struct RadioButtonField_Previews: PreviewProvider {
+    @State var isMarked: true
+    
+    static var previews: some View {
+        RadioButtonField(id: <#String#>, label: <#String#>, size: <#CGFloat#>, color: <#Color#>, textSize: <#CGFloat#>, isMarked: $isMarked
+        )
+    }
+}
+
+
+//let id: String
+//let label: String
+//let size: CGFloat
+//let color: Color
+//let textSize: CGFloat
+//@Binding var isMarked:Bool
 //
-//    
-//    init(
-//        id: String,
-//        label:String,
-//        size: CGFloat = 20,
-//        color: Color = Color.black,
-//        textSize: CGFloat = 14,
-//        isMarked: Bool = false
-//        ) {
-//        self.id = id
-//        self.label = label
-//        self.size = size
-//        self.color = color
-//        self.textSize = textSize
-//        self.isMarked = isMarked
-//    }
-//    
-//    var body: some View {
-//        Button(action:{
 //
-//        }) {
-//            HStack(alignment: .center, spacing: 10) {
-//                Image(systemName: self.isMarked ? "largecircle.fill.circle" : "circle")
-//                    .renderingMode(.original)
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fit)
-//                    .frame(width: self.size, height: self.size)
-//                Text(label)
-//                    .font(Font.system(size: textSize))
-//                Spacer()
-//            }.foregroundColor(self.color)
-//        }
-//        .foregroundColor(Color.white)
-//    }
+//init(
+//    id: String,
+//    label:String,
+//    size: CGFloat = 20,
+//    color: Color = Color.blue,
+//    textSize: CGFloat = 14,
+//    isMarked: Bool = false
+//    ) {
+//    self.id = id
+//    self.label = label
+//    self.size = size
+//    self.color = color
+//    self.textSize = textSize
+//    self.isMarked = isMarked
 //}
 //
-//struct RadioButtonField_Previews: PreviewProvider {
-//    enum Gender: String {
-//        case male = "Male"
-//        case female = "Female"
+//var body: some View {
+//    Button(action:{
+//        isMarked = !isMarked
+//    }) {
+//        HStack(alignment: .center, spacing: 10) {
+//            Image(systemName: self.isMarked ? "largecircle.fill.circle" : "circle")
+//                .renderingMode(.original)
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .frame(width: self.size, height: self.size)
+//
+//
+//            Text(label)
+//                .font(Font.system(size: textSize))
+//
+//            Spacer()
+//        }.foregroundColor(.blue)
 //    }
-//    
-//    
-//    @State var selectedId: String = ""
-//    func radioGroupCallback(id: String) {
-//        selectedId = id
-//    }
-//    
-//    
-//    static var previews: some View {
-//        RadioButtonField(
-//            id: Gender.male.rawValue,
-//            label: Gender.male.rawValue,
-//            isMarked: selectedId == Gender.male.rawValue
-//        )
-//    }
+//    .foregroundColor(Color.blue)
 //}
